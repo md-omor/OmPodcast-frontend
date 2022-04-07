f5cd28e1360b9e93ad825f31549a192b68d3224b
Md Omor <76596385+md-omor@users.noreply.github.com>
2022-04-08 05:19:36 +0600

    Layer 1 protected route create

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import Navigation from "./components/shared/Navigation";
+import Authenticate from "./screens/Authenticate";
 import Home from "./screens/Home";
-import Login from "./screens/Login";
-import Register from "./screens/Register";
+// import Login from "./screens/Login";
+// import Register from "./screens/Register";
+
+const isAuth = false;
 
 function App() {
   return (
     <>
       <Navigation />
       <Routes>
-        <Route path="/" element={<Home />} exact />
-        <Route path="/register" element={<Register />} exact />
-        <Route path="/login" element={<Login />} exact />
+        <Route
+          path="/"
+          element={
+            <GuestRoute>
+              <Home />
+            </GuestRoute>
+          }
+          exact
+        />
+        <Route
+          path="/authenticate"
+          element={
+            <GuestRoute>
+              <Authenticate />
+            </GuestRoute>
+          }
+        />
+        {/* <Route path="/register" element={<Register />} exact />
+        <Route path="/login" element={<Login />} exact /> */}
       </Routes>
     </>
   );
 }
+
+const GuestRoute = ({ children }) => {
+  const location = useLocation();
+  return isAuth ? (
+    <Navigate to="/rooms" state={{ from: location }} replace />
+  ) : (
+    children
+  );
+};
 
 export default App;
---
+++ src/screens/Authenticate.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import StepOtp from "./Steps/StepOtp";
+import StepPhoneEmail from "./Steps/StepPhoneEmail";
+
+const steps = {
+  1: StepPhoneEmail,
+  2: StepOtp,
+};
+
+const Authenticate = () => {
+  const [step, setStep] = useState(1);
+  const Step = steps[step];
+
+  const onNext = () => {
+    setStep(step + 1);
+  };
+  return (
+    <div>
+      <Step onNext={onNext} />
+    </div>
+  );
+};
+
+export default Authenticate;
