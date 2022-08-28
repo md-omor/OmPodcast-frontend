15d787bf2891f5d5eae0c4a5f2825f4ed2af9782
md-omor <developeromor@gmail.com>
2022-08-29 00:52:34 +0600

    Email function rmeove

--- src/screens/Steps/StepPhoneEmail/StepPhoneEmail.js
+++ src/screens/Steps/StepPhoneEmail/StepPhoneEmail.js
@@ -22,14 +22,14 @@ const StepPhoneEmail = ({ onNext }) => {
         >
           <img src="/assets/phone_android.svg" alt="phone" />
         </button>
-        <button
+        {/* <button
           className={`${
             type === "email" ? "bg-[#0077FF]" : "bg-[#262626]"
           }  rounded-xl mr-3 w-14 h-14 flex items-center justify-center`}
           onClick={() => setType("email")}
         >
           <img src="/assets/email.svg" alt="email" />
-        </button>
+        </button> */}
       </div>
 
       <Component onNext={onNext} />
